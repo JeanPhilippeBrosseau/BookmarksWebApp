bb69e06b52a28a933f9c3725b5fc400d68f9225e
Jean-Philippe Brosseau <201489111@edu.clg.qc.ca>
2021-01-30 14:32:59 -0500

    Revert "Add Bookmarks"
    This reverts commit 9875d376e36dfe0a86d382539214806723af4216.

--- BookmarksWebApp/Startup.cs
+++ BookmarksWebApp/Startup.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,8 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using BookmarksWebApp.Data;
 
 namespace BookmarksWebApp
 {
@@ -26,9 +24,6 @@ namespace BookmarksWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
-            services.AddDbContext<BookmarksWebAppContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("BookmarksWebAppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
